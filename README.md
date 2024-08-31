2099af3b4d2be209d01cf53703a7c22fa3df5637
fabioinacio1980 <fabio_inacio@hotmail.com>
2024-08-31 09:39:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-jrfra3
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/fabioinacio1980/stackblitz-starters-jrfra3)
---
+++ index.js
@@ -0,0 +1,17 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+
+const name = {
+        nome: 'fabio',
+        endereco: {
+          rua: 'marfim',
+          numero: 158,
+          cidade: 'ubirata',
+          estado: 'PR',
+          pais: 'Brasil'
+          }
+        sexo: 'masculino'
+}
+
+console.log(name.nome)
